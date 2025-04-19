a1cbccfa613bbe4785540b6937aaebf577893bad
leolbl <leo.ibarralopez@gmail.com>
2025-04-19 15:49:34 -0400

    cache: borrar archivos basura

--- src/screens/editorEmpresasPage.js
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-const EditorEmpresasPage = () => {
-  return <div><h1>Editor Empresas Page</h1></div>;
-};
-
-export default EditorEmpresasPage;
--- src/screens/historiaPage.js
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-const HistoriaPage = () => {
-  return <div><h1>Historia Page</h1></div>;
-};
-
-export default HistoriaPage;
