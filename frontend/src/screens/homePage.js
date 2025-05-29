d8eda45ebb772e4dc720edd9ee7b6aa8982945d9
leandrobaldur <160677582+leandrobaldur@users.noreply.github.com>
2025-05-29 08:09:52 -0400

    ultimo cambio actividades y barra izquierda

--- frontend/src/screens/homePage.js
+++ frontend/src/screens/homePage.js
@@ -30,25 +30,25 @@ const HomePage = () => {
                 icono: "/media/homePage/lightIcon.png",
                 subtitulo: "INNOVACIÓN",
                 imagen: "/media/homePage/c1.webp",
-                texto: "Transformamos ideas en soluciones que marcan la diferencia.",
+                texto: "Más de cuatro décadas impulsando el cambio constante, reinventándonos para anticipar el futuro y ofrecer soluciones que transforman industrias y vidas.",
               },
               {
                 icono: "/media/homePage/recicleIcon.png",
                 subtitulo: "SOSTENIBILIDAD",
                 imagen: "/media/homePage/c2.webp",
-                texto: "Comprometidos con un futuro responsable y respetuoso con el planeta",
+                texto: "Creciendo junto a nuestro entorno, adaptándonos y protegiendo recursos para garantizar un legado duradero y un impacto positivo en las generaciones futuras.",
               },
               {
                 icono: "/media/homePage/secureIcon.png",
                 subtitulo: "CONFIANZA",
                 imagen: "/media/homePage/c3.jpg",
-                texto: "Construimos relaciones sólidas basadas en transparencia y compromiso..",
+                texto: "Construimos relaciones sólidas y duraderas, basadas en la transparencia y el compromiso, que han sido el pilar para acompañar a nuestros clientes por más de cuatro décadas.",
               },
               {
                 icono: "/media/homePage/awardIcon.png",
                 subtitulo: "EXCELENCIA",
                 imagen: "/media/homePage/c4.jpg",
-                texto: "Reconocidos por superar estándares y alcanzar la perfección.",
+                texto: "Más de 40 años superando expectativas y estableciendo estándares de calidad, dedicados a la perfección en cada producto y servicio que ofrecemos.",
               },
             ]}
           />
@@ -57,7 +57,7 @@ const HomePage = () => {
         <ContenedorLateral
           subtitulo="SEMANA DE ACTIVIDADES"
           texto="Explorá las últimas noticias, actualizaciones y promociones disponibles en nuestra plataforma."
-          imagen="/media/homePage/lateral.png"
+          imagen="/media/homePage/work.jpeg"
           ancho="70vw"
           alto="43.5vw"
           ovaloRedondez="20px"
